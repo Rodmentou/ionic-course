1b467bb9cd7821158419548c5f631fa83b10ed37
Rodrigo <rodmentou@gmail.com>
2015-12-17 21:27:52 -0300

    Finished base tutorial

--- code/www/js/app.js
+++ code/www/js/app.js
@@ -15,7 +15,7 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-    
+
 
   });
 })
@@ -30,12 +30,33 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
   $stateProvider
 
 
+  .state('splash', {
+    url: '/',
+    templateUrl: 'templates/splash.html',
+    controller: 'SplashCtrl',
+    onEnter: function ($state, User) {
+      User.checkSession().then( function (hasSession) {
+        if (hasSession) $state.go('tab.discover');
+      });
+    }
+  })
+
   // Set up an abstract state for the tabs directive:
   .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html',
-    controller: 'TabsCtrl'
+    controller: 'TabsCtrl',
+    resolve: {
+      populateSession: function (User) {
+        return User.checkSession();
+      }
+    },
+    onEnter: function ($state, User) {
+      User.checkSession().then( function (hasSession) {
+        if (!hasSession) $state.go('splash');
+      });
+    }
   })
 
   // Each tab has its own nav history stack:
@@ -60,7 +81,7 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
       }
     })
   // If none of the above states are matched, use this as the fallback:
-  $urlRouterProvider.otherwise('/tab/discover');
+  $urlRouterProvider.otherwise('/');
 
 })
 
--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,15 +4,88 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope) {
+.controller('DiscoverCtrl',
+function($scope, $timeout, User, Recommendations, $ionicLoading) {
+
+  var showLoading = function () {
+    $ionicLoading.show({
+      template: '<i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  };
+
+  var hideLoading = function () {
+    $ionicLoading.hide();
+  };
+
+  $scope.nextAlbumImg = function () {
+    if (Recommendations.queue.length > 1) {
+      return Recommendations.queue[1].image_large;
+    }
+
+    return '';
+  };
+
+  $scope.sendFeedBack = function (bool) {
+
+
+    if (bool) User.addSongToFavorites($scope.currentSong);
+
+    $scope.currentSong.rated = bool;
+    $scope.currentSong.hide = true;
+
+    Recommendations.nextSong();
+
+    $timeout( function () {
+      $scope.currentSong = Recommendations.queue[0];
+      $scope.currentSong.loaded = false;
+    }, 250);
+
+    Recommendations.playCurrentSong().then(function () {
+      $scope.currentSong.loaded = true;
+    });
+  };
+
+  Recommendations.init()
+    .then( function () {
+      hideLoading();
+      $scope.currentSong = Recommendations.queue[0];
+      return Recommendations.playCurrentSong();
+  }).then ( function () {
+    hideLoading();
+    $scope.currentSong.loaded = true;
+  });
+
+  showLoading();
 
 })
 
+.controller('SplashCtrl', function ($scope, $state, User) {
+  $scope.submitForm = function (username, signingUp) {
+    User.auth(username, signingUp).then( function () {
+      $state.go('tab.discover');
+    }, function () {
+      alert('Hmm.. try another username.');
+    });
+  };
 
+
+})
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope) {
+.controller('FavoritesCtrl', function($scope, User, $window) {
+
+  $scope.username = User.username;
+
+  $scope.openSong = function (song) {
+    $window.open(song.open_url, "_system");
+  };
+
+  $scope.favorites = User.favorites;
+  $scope.removeSong = function (song, index) {
+    User.removeSongFromFavorites(song, index);
+  };
 
 })
 
@@ -20,6 +93,25 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+.controller('TabsCtrl', function($scope, Recommendations, User, $window) {
+
+  $scope.favCount = User.favoriteCount;
+
+  $scope.logout = function () {
+    User.destroySession();
+
+    $window.location.href = 'index.html';
+  };
+
+  $scope.enteringFavorites = function () {
+    User.newFavorites = 0;
+    Recommendations.haltAudio();
+  };
+
+  $scope.leavingFavorites = function () {
+    Recommendations.init();
+  };
+
+
 
 });
